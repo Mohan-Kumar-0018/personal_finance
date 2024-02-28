abc5c2232dcbccc223e38ae09c9309c625e41db8
Mohan Kumar <mohankumaarrr@gmail.com>
2024-02-28 20:12:16 +0530

    remove unused code

--- process_kotak.py
+++ process_kotak.py
@@ -21,25 +21,26 @@ def read_from_kotak_csv():
     file_path = get_kotak_csv_file_path()
     column_mapping = {'Sl. No.': 'S_NO', 'Transaction Date': 'DATE', 'Description': 'DESCRIPTION', 'Chq / Ref No.': 'TRANSACTION_ID', 'Amount': 'AMOUNT', 'Dr / Cr': 'TYPE', 'Balance': 'BALANCE'}
     columns = column_mapping.keys()
-    data_df = pd.read_csv(file_path, skiprows=13, on_bad_lines='warn')
-    df_filtered = data_df[pd.to_numeric(data_df.iloc[:, 0], errors='coerce').notna()][columns]
-    df_filtered = df_filtered.rename(columns=column_mapping)
-    df_filtered['TYPE'] = df_filtered['TYPE'].map({'DR': 'DEBIT', 'CR': 'CREDIT'})
-    df_filtered['AMOUNT'] = df_filtered['AMOUNT'].str.replace(',', '').astype(float)
-    df_filtered['BALANCE'] = df_filtered['BALANCE'].str.replace(',', '').astype(float)
-    print("df_filtered ------> ", df_filtered)
-    credit_data = df_filtered[df_filtered['TYPE'] == 'CREDIT']
-    print("credit_data ------> ")
-    print(credit_data)
-    debit_data = df_filtered[df_filtered['TYPE'] == 'DEBIT']
-    print("debit_data ------> ")
-    print(debit_data)
+    df = pd.read_csv(file_path, skiprows=13, on_bad_lines='warn')
+    df = df[pd.to_numeric(df.iloc[:, 0], errors='coerce').notna()][columns]
+    df = df.rename(columns=column_mapping)
+    df['TYPE'] = df['TYPE'].map({'DR': 'DEBIT', 'CR': 'CREDIT'})
+    df['AMOUNT'] = df['AMOUNT'].str.replace(',', '').astype(float)
+    df['BALANCE'] = df['BALANCE'].str.replace(',', '').astype(float)
+    df["REMARKS"] = ""
 
-    total_income = credit_data['AMOUNT'].sum()
-    print("total_income ------> ", total_income)
-    total_expenses = debit_data['AMOUNT'].sum()
-    print("total_expenses ------> ", total_expenses)
-    
+    save_kotak_csv(df, "all_data")
+    credit_data = df[df['TYPE'] == 'CREDIT']
+    save_kotak_csv(credit_data, "credit_data")
+    debit_data = df[df['TYPE'] == 'DEBIT']
+    save_kotak_csv(debit_data, "debit_data")
+
+
+def save_kotak_csv(data_frame, file_name):
+    current_folder = main_folder + month_folder + "/kotak/"
+    output_file_path = current_folder + file_name + '.csv'
+    print("output_file_path = ", output_file_path)
+    data_frame.to_csv(output_file_path, index=False)
 
 def process_kotak_data():
     print("process_kotak_data started ...")
