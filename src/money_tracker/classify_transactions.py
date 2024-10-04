6a5c39f2a76db8a35adae420ba1171e8cb9a7638
Mohan Kumar M <mohankumar@shopup.org>
2024-10-05 04:19:13 +0530

    minor code restructure

--- src/money_tracker/classify_transactions.py
+++ src/money_tracker/classify_transactions.py
@@ -5,60 +5,60 @@ import pandas as pd
 def classify_credit_transactions(banks_credits_df):
   banks_credits_df['IS_INCOME'] = ''
   for index, row in banks_credits_df.iterrows():
-    # NACH-DB is a transaction from Stocks Dividends
-    if str(row['DESCRIPTION']).startswith('NACH-') & str(row['TRANSACTION_ID']).startswith('NACHDB'):
+    if is_income(row):
       banks_credits_df.at[index, 'IS_INCOME'] = 'YES'
       continue
-      
-    # Kotak Savings Account Interest
-    kotak_interest_pattern = r"^Int\.Pd:(\d+):(\d{2}-\d{2}-\d{4}) to (\d{2}-\d{2}-\d{4})$"
-    match = re.match(kotak_interest_pattern, str(row['DESCRIPTION']))
-    if match and (pd.isna(row['TRANSACTION_ID']) or row['TRANSACTION_ID'] == '') and 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
-      banks_credits_df.at[index, 'IS_INCOME'] = 'YES'
-      continue
-
-    # Kotak Salary:
-    salary_pattern = r"^NEFT.*SHOPUP INDIA TECHNOLOGIES.*$"
-    match = re.match(salary_pattern, str(row['DESCRIPTION']))
-    if match and 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
-      banks_credits_df.at[index, 'IS_INCOME'] = 'YES'
-      continue
-
-    # PNB Savings Account Interest
-    pnb_interest_pattern = r"^(\d+):Int\.Pd:(\d{2}-\d{2}-\d{4}) to (\d{2}-\d{2}-\d{4})$"
-    match = re.match(pnb_interest_pattern, str(row['DESCRIPTION']))
-    if match and (pd.isna(row['TRANSACTION_ID']) or row['TRANSACTION_ID'] == '') and 'PNB'.lower() in str(row['ACCOUNT']).lower():
-      banks_credits_df.at[index, 'IS_INCOME'] = 'YES'
-      continue
-
+  
   return banks_credits_df
 
 def classify_debit_transactions(banks_debits_df):
   banks_debits_df['IS_INVESTMENT'] = ''
   banks_debits_df['IS_EXPENSE'] = ''
   for index, row in banks_debits_df.iterrows():
-      # SVV GOLD Plan
-      svv_gold_plan_pattern = r".*SRI VALLI VILAS.*"
-      match = re.match(svv_gold_plan_pattern, str(row['DESCRIPTION']))
-      if match:
-        banks_debits_df.at[index, 'IS_INVESTMENT'] = 'YES'
-        continue
-
-      if 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
-        # Kotak INDMoney
-        indmoney_pattern = r".*INDMoney.*"
-        match = re.match(indmoney_pattern, str(row['DESCRIPTION']))
-        if match:
-          banks_debits_df.at[index, 'IS_INVESTMENT'] = 'YES'
-          continue
-
-        # Zerodha
-        zerodha_pattern = r".*Zerodha Broking.*"
-        match = re.match(zerodha_pattern, str(row['DESCRIPTION']))
-        if match:
-          banks_debits_df.at[index, 'IS_INVESTMENT'] = 'YES'
-          continue
-
-      
+    if is_investment(row):
+      banks_debits_df.at[index, 'IS_INVESTMENT'] = 'YES'
+      continue
 
   return banks_debits_df
+
+def is_income(row):
+  # NACH-DB is a transaction from Stocks Dividends
+  if str(row['DESCRIPTION']).startswith('NACH-') and str(row['TRANSACTION_ID']).startswith('NACHDB'):
+    return True
+    
+  # Kotak Savings Account Interest
+  kotak_interest_pattern = r"^Int\.Pd:(\d+):(\d{2}-\d{2}-\d{4}) to (\d{2}-\d{2}-\d{4})$"
+  if re.match(kotak_interest_pattern, str(row['DESCRIPTION'])) and (pd.isna(row['TRANSACTION_ID']) or row['TRANSACTION_ID'] == '') and 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
+    return True
+
+  # Kotak Salary
+  salary_pattern = r"^NEFT.*SHOPUP INDIA TECHNOLOGIES.*$"
+  if re.match(salary_pattern, str(row['DESCRIPTION'])) and 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
+    return True
+
+  # PNB Savings Account Interest
+  pnb_interest_pattern = r"^(\d+):Int\.Pd:(\d{2}-\d{2}-\d{4}) to (\d{2}-\d{2}-\d{4})$"
+  if re.match(pnb_interest_pattern, str(row['DESCRIPTION'])) and (pd.isna(row['TRANSACTION_ID']) or row['TRANSACTION_ID'] == '') and 'PNB'.lower() in str(row['ACCOUNT']).lower():
+    return True
+
+  return False
+
+
+def is_investment(row):
+  # SVV GOLD Plan
+  svv_gold_plan_pattern = r".*SRI VALLI VILAS.*"
+  if re.match(svv_gold_plan_pattern, str(row['DESCRIPTION'])):
+    return True
+
+  if 'KOTAK'.lower() in str(row['ACCOUNT']).lower():
+    # Kotak INDMoney
+    indmoney_pattern = r".*INDMoney.*"
+    if re.match(indmoney_pattern, str(row['DESCRIPTION'])):
+      return True
+
+    # Zerodha
+    zerodha_pattern = r".*Zerodha Broking.*"
+    if re.match(zerodha_pattern, str(row['DESCRIPTION'])):
+      return True
+
+  return False
